7d6b46cf8b8c8e760a05f03821b218d033965f33
Doni Setiawan <donisetiawan.050@gmail.com>
2020-08-18 13:09:12 +0700

    Adding todo items

--- src/components/todoApp.js
+++ src/components/todoApp.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Mutation } from '@apollo/client/react/components';
+import { ADD_TODO, GET_USER } from '../constants';
+import TodoTextInput from './todoTextInput';
 
-function TodoApp() {
+function TodoApp({ user }) {
+  console.log(user);
   return (
     <div>
-      TodoApp
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+
+          <Mutation
+            mutation={ADD_TODO}
+            refetchQueries={[
+              {
+                query: GET_USER,
+                variables: { userId: 'me' },
+              },
+            ]}
+          >
+            {(addTodo) => (
+              <TodoTextInput
+                className="new-todo"
+                onSave={(text) => addTodo({ variables: { text } })}
+                placeholder="What needs to be done?"
+              />
+            )}
+          </Mutation>
+        </header>
+      </section>
     </div>
   );
 }
